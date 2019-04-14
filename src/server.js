e0a7aeb8b164a40b0fd171c2afb012c3e627c2a1
Carlos Levir <carloslevirfs@gmail.com>
2019-04-14 13:45:28 -0300

    Socket.io and Cors Configs.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,41 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-mongoose.connect(`mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0-afdwt.mongodb.net/dropbox?retryWrites=true`, {
-  useNewUrlParser: true
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  socket.on('connectRoom', (box) => {
+    socket.join(box);
+  });
+});
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DATABASE_USERNAME}:${
+    process.env.DATABASE_PASSWORD
+  }@cluster0-afdwt.mongodb.net/dropbox?retryWrites=true`,
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(require("./routes"));
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
-app.listen(3333);
+app.use(require('./routes'));
+
+server.listen(process.env.PORT || 3333);
