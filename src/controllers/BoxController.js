eab7518764c1de3599d53ba2e189865cd5220ba4
Carlos Levir <carloslevirfs@gmail.com>
2019-04-14 13:44:55 -0300

    Add of Show Method in BoxController.

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -2,7 +2,16 @@ const Box = require('../models/Box');
 
 class BoxController {
   async store(req, res) {
-    const box = await Box.create({ title: req.body.title })
+    const box = await Box.create({ title: req.body.title });
+
+    return res.json(box);
+  }
+
+  async show(req, res) {
+    const box = await Box.findById(req.params.id).populate({
+      path: 'files',
+      options: { sort: { createdAt: -1 } }
+    });
 
     return res.json(box);
   }
