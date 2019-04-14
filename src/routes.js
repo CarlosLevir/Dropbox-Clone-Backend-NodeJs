06691cbf92933c923b258e80ab662d03cc43f486
Carlos Levir <carloslevirfs@gmail.com>
2019-04-14 13:45:05 -0300

    Routes Config.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+const multerConfig = require('./config/multer');
+
 const BoxController = require('./controllers/BoxController');
+const FileController = require('./controllers/FileController');
+
 const routes = express.Router();
 
 routes.post('/boxes', BoxController.store);
+routes.get('/boxes/:id', BoxController.show);
+
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store);
 
 module.exports = routes;
